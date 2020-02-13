3237527daff09f3d238ca8e9dc4c77a7dc541ccf
Blaise <baldr51@protonmail.com>
2020-02-13 14:38:29 +0100

    Mod 5.4 start Change scripts.js & index.html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
-printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to' + playerMove + ', to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Random digit is: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'slap';
+}
+
+else if(randomNumber == 2){
+  computerMove = 'punch'
+}
+
+else if(randomNumber == 3){
+  computerMove = 'bat'
+}
+
+printMessage('My move is: ' + computerMove);
