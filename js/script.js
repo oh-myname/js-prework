d8a46cf432d9e9f1ba8a5d4b492e2e0df9531ab4
Blaise <baldr51@protonmail.com>
2020-03-02 20:30:06 +0100

    Complete 5.5

--- js/script.js
+++ js/script.js
@@ -1,20 +1,45 @@
+function getMoveName(argMoveId){
+  if(argMoveId == 1){
+    return 'slap';
+  }
+    else if(argMoveId == 2){
+    return 'punch';
+  }
+    else if(argMoveId == 3){
+    return 'bat';
+  }
+    else{
+    printMessage('I do not know what is ' + argMoveId + '.');
+    return 'unknown move';
+  }
+}
+
+function displayResult(argComputerMove, argPlayerMove){
+  printMessage('My move is ' + argComputerMove + ' , your move is ' + argPlayerMove);
+  console.log('moves:', argComputerMove, argPlayerMove);
+
+  if(argComputerMove == 'slap' && argPlayerMove == 'punch'){
+    printMessage('You win!');
+  }
+    else if(argComputerMove == 'punch' && argPlayerMove == 'bat'){
+    printMessage('You win!');
+  }
+    else if(argComputerMove == 'slap' && argPlayerMove == 'bat'){
+    printMessage('You win!');
+}
+    else if(argComputerMove == argPlayerMove){
+    printMessage('Draw!');
+  }
+    else {
+    printMessage('You lose!');
+  }
+}
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Random digit is: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
-
-if(randomNumber == 1){
-  computerMove = 'slap';
-}
-
-else if(randomNumber == 2){
-  computerMove = 'punch'
-}
-
-else if(randomNumber == 3){
-  computerMove = 'bat'
-}
+let computerMove = getMoveName(randomNumber);
 
 printMessage('My move is: ' + computerMove);
 
@@ -22,46 +47,8 @@ let playerInput = prompt('Choose your move! 1: Give a slap to pc, 2: Punch pc, 3
 
 console.log('You would love to : ' + playerInput);
 
-let playerMove = 'start learning new things, like counting from one to three. Your move is not recognized by';
-
-if(playerInput == '1'){
-  playerMove = 'slap';
-}
-
-else if(playerInput == '2'){
-  playerMove = 'punch'
-}
-
-else if(playerInput == '3'){
-  playerMove='bat'
-}
+let playerMove = getMoveName(playerInput);
 
 printMessage('You would love to ' + playerMove + ' your pc');
 
-if(computerMove == 'slap' && playerMove == 'slap'){
-  printMessage('Draw!');
-}
-
-else if(computerMove == 'punch' && playerMove == 'punch'){
-  printMessage('Draw!');
-}
-
-else if(computerMove == 'bat' && playerMove == 'bat'){
-  printMessage('Draw!');
-}
-
-else if(computerMove == 'slap' && playerMove == 'punch'){
-  printMessage('You win!');
-}
-
-else if(computerMove == 'slap' && playerMove == 'bat'){
-  printMessage('You win!');
-}
-
-else if(computerMove == 'punch' && playerMove == 'slap'){
-  printMessage('Pc wins!');
-}
-
-else if(computerMove == 'bat' && playerMove == 'punch'){
-  printMessage('Pc wins!');
-}
+displayResult(playerMove, computerMove);
